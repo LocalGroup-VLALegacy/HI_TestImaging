0c6b41a19890a043598e13317a8024a719bb2497
e-koch <koch.eric.w@gmail.com>
2020-11-03 16:53:51 -0500

    Add in looping over with and without A config

--- manual_imaging_HI_allconfigs.py
+++ manual_imaging_HI_allconfigs.py
@@ -13,6 +13,7 @@
 from tasks import tclean
 
 import os
+from copy import copy
 
 basepath = "/mnt/space/ekoch/VLA_tracks/"
 
@@ -34,74 +35,84 @@ run_deepclean = False
 
 # Everything in one:
 
+with_Aconfig = [True, False]
+
 with_contsub = [False, True]
 weightings = ['natural', 'robust']
 tapers = ['', ['5arcsec']]
 
-for contsub in with_contsub:
+for has_Aconfig in with_Aconfig:
 
-    if contsub is True:
-        myvis = vis_contsubs
-        image_str = 'contsub'
-    else:
-        myvis = vis_nocontsubs
-        image_str = 'nocontsub'
+    for contsub in with_contsub:
 
-    for weight in weightings:
+        if contsub is True:
+            myvis = vis_contsubs
+            image_str = 'contsub'
+        else:
+            myvis = vis_nocontsubs
+            image_str = 'nocontsub'
 
-        for taper in tapers:
+        # Remove the last track from A config to compare w/ and w/o A tracks
+        if has_Aconfig:
+            pass
+        else:
+            myvis = copy(myvis)[:-1]
 
-            print("On {0} {1} {2}".format(weight, taper, image_str))
+        for weight in weightings:
 
-            imagename_run = os.path.join(datapath,
-                                         'M31_Field17_ABCD_{0}_{1}_{2}'.format(weight, taper, image_str))
+            for taper in tapers:
 
-            # Dirty map.
-            if run_dirtyimaging:
+                print("On {0} {1} {2} Includes A conf {3}".format(weight, taper, image_str, has_Aconfig))
 
-                tclean(vis=vis_contsubs,
-                    field='M33',
-                    spw='0',
-                    intent='OBSERVE_TARGET#ON_SOURCE',
-                    datacolumn='corrected',
-                    imagename=imagename_run,
-                    imsize=4096,
-                    cell='0.5arcsec',
-                    phasecenter='',  # Only one field
-                    nchan=27,
-                    start='-250km/s',
-                    width='10km/s',
-                    specmode='cube',
-                    outframe='LSRK',
-                    gridder='mosaic',
-                    chanchunks=-1,
-                    mosweight=True,
-                    pblimit=0.5,
-                    pbmask=0.5,
-                    deconvolver='multiscale',
-                    scales=[0, 5, 10],
-                    restoration=True,
-                    pbcor=False,
-                    weighting='briggs',
-                    robust=0.5,
-                    niter=0,
-                    cycleniter=100,  # Force many major cycles
-                    nsigma=4.,
-                    usemask='auto-multithresh',
-                    sidelobethreshold=1.0,
-                    noisethreshold=3.0,
-                    lownoisethreshold=1.5,
-                    negativethreshold=0.0,
-                    minbeamfrac=0.1,
-                    growiterations=75,
-                    dogrowprune=True,
-                    minpercentchange=1.0,
-                    fastnoise=False,
-                    threshold='',
-                    interactive=0,
-                    savemodel='none',
-                    parallel=False,
-                    calcres=True,
-                    calcpsf=True,
-                    smallscalebias=0.0,
-                    )
+                imagename_run = os.path.join(datapath,
+                                            'M31_Field17_ABCD_{0}_{1}_{2}'.format(weight, taper, image_str))
+
+                # Dirty map.
+                if run_dirtyimaging:
+
+                    tclean(vis=vis_contsubs,
+                        field='M33',
+                        spw='0',
+                        intent='OBSERVE_TARGET#ON_SOURCE',
+                        datacolumn='corrected',
+                        imagename=imagename_run,
+                        imsize=4096,
+                        cell='0.5arcsec',
+                        phasecenter='',  # Only one field
+                        nchan=27,
+                        start='-250km/s',
+                        width='10km/s',
+                        specmode='cube',
+                        outframe='LSRK',
+                        gridder='mosaic',
+                        chanchunks=-1,
+                        mosweight=True,
+                        pblimit=0.5,
+                        pbmask=0.5,
+                        deconvolver='multiscale',
+                        scales=[0, 5, 10],
+                        restoration=True,
+                        pbcor=False,
+                        weighting='briggs',
+                        robust=0.5,
+                        niter=0,
+                        cycleniter=100,  # Force many major cycles
+                        nsigma=4.,
+                        usemask='auto-multithresh',
+                        sidelobethreshold=1.0,
+                        noisethreshold=3.0,
+                        lownoisethreshold=1.5,
+                        negativethreshold=0.0,
+                        minbeamfrac=0.1,
+                        growiterations=75,
+                        dogrowprune=True,
+                        minpercentchange=1.0,
+                        fastnoise=False,
+                        threshold='',
+                        interactive=0,
+                        savemodel='none',
+                        parallel=False,
+                        calcres=True,
+                        calcpsf=True,
+                        smallscalebias=0.0,
+                        )
